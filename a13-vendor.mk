60c68e976e9707ff90d51d6d98b880680b509646
SuperAviation001 <superaviation001@gmail.com>
2026-01-19 16:07:50 +1100

    a13: Initialise vendor

---
+++ a13-vendor.mk
@@ -0,0 +1,11 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/samsung/a13
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/a13/proprietary/vendor/etc/nfc/sec_s3nrn4v_hwreg.bin:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/sec_s3nrn4v_hwreg.bin \
+    vendor/samsung/a13/proprietary/vendor/etc/nfc/sec_s3nrn4v_swreg.bin:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/sec_s3nrn4v_swreg.bin \
+    vendor/samsung/a13/proprietary/vendor/firmware/nfc/sec_s3nrn4v_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nfc/sec_s3nrn4v_firmware.bin
